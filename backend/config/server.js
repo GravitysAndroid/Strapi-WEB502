c248794d87bb61c09b9b5c67bc9012ff1a994a7d
Hayden <55818671+GravitysAndroid@users.noreply.github.com>
2021-10-18 09:45:26 +1300

    Created backend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '52078f8b957ab6f678f51821f527be4d'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4edc92fc-d993-48ed-8b9f-ae76b98fcf97'
+};
